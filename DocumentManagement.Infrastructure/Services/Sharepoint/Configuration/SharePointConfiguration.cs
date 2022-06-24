49d9c5080dd5bfe58f5762bbfba36107241f53c8
Bruno Willian <bruno.willian.ads@gmail.com>
2022-06-24 22:35:53 +0100

    Changing configuration

--- DocumentManagement.Infrastructure/Services/Sharepoint/Configuration/SharePointConfiguration.cs
+++ DocumentManagement.Infrastructure/Services/Sharepoint/Configuration/SharePointConfiguration.cs
@@ -1,9 +1,21 @@
-﻿namespace DocumentManagement.Infrastructure.Services.Sharepoint.Configuration
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DocumentManagement.Infrastructure.Services.Sharepoint.Configuration
 {
     public class SharePointConfiguration : ISharePointConfiguration
     {
-        public Uri SiteURL { get; } = new Uri("https://brunotwdev.sharepoint.com/sites/brunotw");
-        public string Password { get; } = "O3eikpxf!";
-        public string User { get; } = "brunotw@brunotwdev.onmicrosoft.com";
+        public Uri SiteURL { get; }
+        public string Password { get; }
+        public string User { get; }
+        private readonly IConfiguration _configuration;
+
+        public SharePointConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            SiteURL = new Uri(_configuration.GetValue<string>("SharePointConfig:URL"));
+            Password = _configuration.GetValue<string>("SharePointConfig:Password");
+            User = _configuration.GetValue<string>("SharePointConfig:User");
+        }
     }
 }
