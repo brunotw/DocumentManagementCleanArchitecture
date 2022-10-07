2bd2304cd85b9f59b766443f3727780d6990aef1
Bruno Willian <bruno.willian.ads@gmail.com>
2022-10-07 17:02:32 +0100

    DocumentManagement.MVC.Client project added

---
+++ DocumentManagement.MVC.Client/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using DocumentManagement.MVC.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DocumentManagement.MVC.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Documents()
+        {
+            string url = "https://localhost:7175/api/document/download/file1.txt";
+            using (HttpClient client = new HttpClient())
+            {
+                var result =  await client.GetStringAsync(url);
+            }
+
+
+            List<DocumentViewModel> documents = new List<DocumentViewModel>
+            {
+                new DocumentViewModel{FIleName= "File1", Extension= ".txt" },
+                new DocumentViewModel{FIleName= "File2", Extension= ".txt" },
+            };
+
+            return View(documents);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DocumentManagement.MVC.Client/Models/DocumentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DocumentManagement.MVC.Client.Models
+{
+    public class DocumentViewModel
+    {
+        public string FIleName { get; set; }
+        public string Extension { get; set; }
+    }
+}
